2c0a41a55eba528031ab313472420f7164fea0d6
Alex Dekau <alex.potatofarms@gmail.com>
2026-03-12 00:28:45 -0400

    fix duality

--- order-lattices/OrderAndLattices.lean
+++ order-lattices/OrderAndLattices.lean
@@ -345,12 +345,46 @@ theorem inf_comm {α : Type} [Lattice α] {a b : α} :
     · apply Lattice.inf_le_right
     · apply Lattice.inf_le_left
 
+-- 3.2 Duality
+structure Dual (α : Type) where
+  val : α
+
+@[simp] theorem Dual.mk_inj {α : Type} {a b : α} :
+    (⟨a⟩ : Dual α) = ⟨b⟩ ↔ a = b := by
+  constructor
+  · exact congrArg Dual.val
+  · exact congrArg Dual.mk
+
+-- 1. The dual order flips ≤
+instance {α  : Type} [LE α] : LE (Dual α) where
+  le a b := LE.le (α := α) b.val a.val   -- a ≤_dual b means b ≤_original a
+
+-- 2. The dual partial order (le_trans flips h1/h2, antisymm flips arguments)
+instance {α : Type} [LE α] [PartialOrder α] : PartialOrder (Dual α) where
+  le_refl {a}       := PartialOrder.le_refl a.val
+  le_antisymm {a b} := fun h1 h2 => by
+    cases a; cases b;
+    congr 1
+    exact PartialOrder.le_antisymm (α := α) h2 h1
+  le_trans {a b c}  := fun h1 h2 => by
+    cases a; cases b; cases c
+    exact PartialOrder.le_trans (α := α) h2 h1
+
+instance {α : Type} [Lattice α] : Lattice (Dual α) where
+  inf           := fun ⟨a⟩ ⟨b⟩  => ⟨Lattice.sup (α := α) a b⟩
+  sup           := fun ⟨a⟩ ⟨b⟩ => ⟨Lattice.inf (α := α) a b⟩
+  inf_le_left   :=fun ⟨a⟩ ⟨b⟩ => Lattice.sup_le_left (α := α) a b
+  inf_le_right  := fun ⟨a⟩ ⟨b⟩  => Lattice.sup_le_right (α := α) a b
+  sup_le_left   := fun ⟨a⟩ ⟨b⟩ => Lattice.inf_le_left (α := α) a b
+  sup_le_right  := fun ⟨a⟩ ⟨b⟩ => Lattice.inf_le_right (α := α) a b
+  le_inf        := fun ⟨a⟩ ⟨b⟩ ⟨c⟩ h1 h2 => by
+    change Lattice.sup (α := α) b c ≤ a
+    exact Lattice.le_sup (α := α) b c a h1 h2
+  le_sup        := fun ⟨a⟩ ⟨b⟩ ⟨c⟩ h1 h2 => by
+    change LE.le (α := α) c (Lattice.inf (α := α) a b)
+    exact Lattice.le_inf (α := α) c a b h1 h2
+
 theorem sup_comm {α : Type} [Lattice α] {a b : α} :
     a ⊔ b = b ⊔ a := by
-  apply PartialOrder.le_antisymm
-  · apply Lattice.le_sup
-    · apply Lattice.sup_le_right
-    · apply Lattice.sup_le_left
-  · apply Lattice.le_sup
-    · apply Lattice.sup_le_right
-    · apply Lattice.sup_le_left
+  have h := @inf_comm (Dual α) _ ⟨a⟩ ⟨b⟩
+  rwa [Dual.mk_inj] at h
